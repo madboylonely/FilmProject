9a08b3c1a7f6dea66dc208211a10461fbd51d06e
longtd <duclongit95@gmail.com>
2019-12-03 18:31:28 +0700

    header carousel

--- film-project/src/component/carousel.js
+++ film-project/src/component/carousel.js
@@ -1,11 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class Carousel extends Component {
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+      <div className="carousel">
+        <div id="myCarousel" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li
+              data-target="#myCarousel"
+              data-slide-to={0}
+              className="active"
+            />
+            <li data-target="#myCarousel" data-slide-to={1} />
+            <li data-target="#myCarousel" data-slide-to={2} />
+            <li data-target="#myCarousel" data-slide-to={3} />
+            <li data-target="#myCarousel" data-slide-to={4} />
+          </ol>
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active">
+              <img
+                className="d-block w-100"
+                src="./img/carousel-1.jpg"
+                alt="First slide"
+              />
             </div>
-        );
-    };
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="./img/carousel-2.jpg"
+                alt="Second slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="./img/carousel-3.jpg"
+                alt="Third slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="./img/carousel-4.jpg"
+                alt="Fourth slide"
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="./img/carousel-5.jpg"
+                alt="Fifth slide"
+              />
+            </div>
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#myCarousel"
+            role="button"
+            data-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#myCarousel"
+            role="button"
+            data-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
--- film-project/src/component/navbar.js
+++ film-project/src/component/navbar.js
@@ -1,11 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+          <a className="navbar-brand ml-3" href="index.html">
+            <img src="img/Logo.png" alt="#" />
+          </a>
+          <button
+            className="navbar-toggler d-lg-none"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavId">
+            <ul className="navbar-nav m-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="index.html">
+                  Trang Chủ
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="index.html">
+                  Lịch Chiếu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="index.html">
+                  Cụm rạp
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="index.html">
+                  Liên hệ
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="right">
+            <a
+              href="index.html"
+              className="pr-3 d-flex align-items-center justify-content-center"
+            >
+              <i className="fa fa-user-circle text-muted" />
+              <span className="text-muted d-inline-block ml-2">Đăng Nhập</span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
